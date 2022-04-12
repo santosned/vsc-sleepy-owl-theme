25941505c990d1e1814c6f07f03c0d87c7337d07
Eduardo M. Santos <99996904+santosned@users.noreply.github.com>
2022-04-12 09:55:09 -0300

    build: add Transpiler and initialize theme generate

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,26 @@
 'strict';
 
-
 const { getThemeInfo } = require('./utils/ThemeDevUtils');
+const { Transpiler } = require('./generate');
 
 getThemeInfo(process.env.npm_package_json)
     .then((themeInfo) => {
-        console.log(themeInfo);
+        /*
+         * Create new theme transpiler @param 'tabWidth' - Define the indentantion tab
+         * width for the generated *-color-theme.json
+         */
+        const transpiler = new Transpiler();
+
+        /*
+         * Generate theme inside ./themes/
+         * @param 'object' - { label:string, src:string, dist:string } which dist and
+         * src are required options.
+         * @param 'callback' - Receives a callback function which return either an
+         * error or undefined when success.
+         */
+        transpiler.generate(themeInfo, (error) => {
+            console.log(error);
+        });
     })
     .catch((err) => {
         if (err && err.message) {
