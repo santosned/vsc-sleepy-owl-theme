2adfb7624b49fc2275a4c20b8967b0c5ef28707e
Eduardo M. Santos <99996904+santosned@users.noreply.github.com>
2022-04-12 09:31:54 -0300

    build: add initial generate.js script features
    Add Transpiler, main goal is to transform the .yaml file into a valid
    .json file used by the vsce to create the theme package.
    Feat: Cleans the json files from unwated entries, such as entries with
    empty or null values.
    
    Add getThemeSchema, it will get the theme file from themes/schemas/ and
    parse it into a object that will be later consumed by the Transpiler.

---
+++ scripts/generate.js
@@ -0,0 +1,80 @@
+'strict';
+
+const jsYaml = require('js-yaml');
+
+const { readThemeSchema, writeThemeSchema } = require('./utils/ThemeDevUtils');
+
+async function getThemeSchema(src) {
+    try {
+        const yamlData = await readThemeSchema(src);
+
+        /*
+         * Parses yaml file into either a plain object, a string, a number, null or
+         * undefined, or throws YAMLException on error.
+         */
+        let jsonData = await jsYaml.load(yamlData, {
+            schema: jsYaml.DEFAULT_SCHEMA,
+            json: true,
+        });
+
+        return Promise.all([jsonData, yamlData]);
+    } catch (err) {
+        if (err && err.message) {
+            console.log(`${err.message}\n`);
+        }
+        return Promise.reject(err);
+    }
+}
+
+class Transpiler {
+    constructor(args) {
+        let { tabWidth } = { ...args };
+        this.tabWidth = typeof tabWidth === 'number' ? tabWidth : 2;
+        this.replacer = (key, value) => {
+            // Exclude json entry if key is either name, author, or version.
+            if (key === 'name' || key === 'author' || key === 'version') {
+                return undefined;
+            }
+            // Exclude json entry if value is null or empty string
+            if (value === null || !value) {
+                return undefined;
+            }
+            return value;
+        };
+    }
+    generate(options, callback) {
+        // Get the required dist and src paths from options
+        const { dist, src } = { ...options };
+        // Throws error if src path does not contain .yml
+        if (src.contains('.yml')) {
+            throw `generate(): Invalid options 'src: <string>'. Received '${src}'.`;
+        }
+
+        // Throws error if dist path does not contain *-color-theme.json
+        if (dist.contains('-color-theme.json')) {
+            throw `generate(): Invalid options 'dist: <string>'. Received '${dist}'.`;
+        }
+
+        getThemeSchema(src)
+            .then(async ([jsonData, yamlData]) => {
+                try {
+                    jsonData = await JSON.stringify(
+                        jsonData,
+                        this.replacer,
+                        this.tabWidth,
+                    );
+
+                    const write = await writeThemeSchema(dist, jsonData);
+                    callback(write);
+                } catch (err) {
+                    console.log(err);
+                    callback(err);
+                }
+            })
+            .catch((err) => {
+                callback(err);
+            });
+    }
+}
+
+module.exports = { Transpiler };
