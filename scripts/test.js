826613f537459078898aaf4d3b6ad66ce3bd30fa
Eduardo M. Santos <99996904+santosned@users.noreply.github.com>
2022-04-14 14:50:06 -0300

    test: add test for theme-color tokens.

--- scripts/test.js
+++ scripts/test.js
@@ -1,11 +1,29 @@
 'strict';
-const { ColorThemeReference, TestColorThemes } = require('./utils/ThemeTestUtils');
+
+const {
+    ColorThemeReference,
+    TestColorThemes,
+} = require('./utils/ThemeTestUtils');
 
 const colorThemeReference = new ColorThemeReference();
 
-colorThemeReference.getData({ update: false }).then((data) => {
-    // console.log('callback: ', data);
-    const testColorThemes = new TestColorThemes();
+/**
+ * Get color theme references from 'themes/schemas/colors.json' in the root folder
+ * or from 'https://code.visualstudio.com/api/references/theme-color' 
+ * @param {boolean} latest Update theme references or use cache
+ * 
+ * Note:
+ * Only set { latest: false } to true if cached data is outdated otherwise keep it false.
+ */
+colorThemeReference.get({ latest: false }).then((data) => {
+    const colorsReference = data.colors;
+    const colorsAvailable = data.colors.length;
 
-    testColorThemes.initialize()
+    const testColorThemes = new TestColorThemes({
+        colors: colorsReference,
+        count: colorsAvailable,
+    });
+    
+    // Test the colors tokens of all color-theme inside 'themes/' directory.
+    testColorThemes.initialize();
 });
--- scripts/utils/ThemeTestUtils.js
+++ scripts/utils/ThemeTestUtils.js
@@ -1,9 +1,21 @@
+'strict';
+
 const https = require('https');
 const fsPromises = require('fs/promises');
+const { readFileSync } = require('fs');
 const { join } = require('path');
 
-const getThemesUrl = (url) => join(__dirname, '../../themes/', url);
+/**
+ *
+ * @param {string} filename theme filename inside themes/ folder
+ * @returns {string}
+ */
+const getThemesUrl = (filename) => join(__dirname, '../../themes/', filename);
 
+/**
+ * Read files inside 'themes/' searching for all '*-color-theme.json' files
+ * @returns List with the location of all valid filenames
+ */
 async function getThemesDir() {
     try {
         const url = join(__dirname, '../../themes/');
@@ -12,30 +24,42 @@ async function getThemesDir() {
             name.includes('-color-theme.json'),
         );
         themesDir = themesDir.map((t) => getThemesUrl(t));
-        return themesDir;
+        return Promise.resolve(themesDir);
     } catch (err) {
         return Promise.reject(err);
     }
 }
 
-async function readColors(url) {
+/**
+ *
+ * @param {string} url The full path of a valid color theme file
+ * @returns {Object} The data inside the color theme file
+ */
+async function readThemeFile(url) {
     try {
         const getData = await fsPromises.readFile(url, 'utf-8');
-        return getData;
+        const data = await JSON.parse(getData);
+        return Promise.all([data, getData]);
     } catch (err) {
         return Promise.reject(err);
     }
 }
 
+/**
+ *
+ * @param {string} htmlData The DOM data received from https://code.visualstudio.com/api/references/theme-color
+ * @returns {string} Colors list with all tokens received from API
+ */
 async function parseTokenColors(htmlData) {
     // Convert html data into array
     htmlData = htmlData.split('\n');
 
-    // Get the colors tokens from html data
+    // Get all data inside a code tag
     let colors = htmlData
         .map((key) => key.slice(key.indexOf('<code>'), key.indexOf('</code>')))
         .map((key) => key.replace('<code>', ''))
         .filter((key) => key !== '');
+
     // Get rid of unwanted tokens
     colors = colors.filter(
         (key) =>
@@ -52,44 +76,50 @@ async function parseTokenColors(htmlData) {
     return colors;
 }
 
-async function getReferenceSchema(options) {
-    return new Promise((resolve, reject) => {
-        https
-            .get(options.url, (res) => {
-                let data = '';
-                res.setEncoding(options.encodeType);
-
-                res.on('data', (resData) => {
-                    data += resData;
-                });
-
-                res.on('end', () => {
-                    resolve(parseTokenColors(data));
-                });
-
-                res.on('error', (err) => {
-                    reject(err);
-                });
-            })
-            .on('error', (err) => {
-                reject(err);
-            });
-    });
-}
-
 class ColorThemeReference {
     constructor() {
         this.src = getThemesUrl('schemas/colors.json');
         this.url = 'https://code.visualstudio.com/api/references/theme-color';
         this.encodeType = 'utf8';
-    }
-    async getData(options) {
-        const { update } = { ...options };
+        /**
+         * Get reference tokens from theme-color api.
+         * @returns {string} Theme color references
+         */
+        this.getThemeColorReference = async ({ url, encodeType }) => {
+            return new Promise((resolve, reject) => {
+                https
+                    .get(url, (res) => {
+                        let data = '';
+                        res.setEncoding(encodeType);
 
-        if (!update) {
+                        res.on('data', (resData) => {
+                            data += resData;
+                        });
+
+                        res.on('end', () => {
+                            resolve(parseTokenColors(data));
+                        });
+
+                        res.on('error', (err) => {
+                            reject(err);
+                        });
+                    })
+                    .on('error', (err) => {
+                        reject(err);
+                    });
+            });
+        };
+    }
+    /**
+     * Get theme-color references
+     * @param {boolean} latest If the color theme references should be update
+     * @returns {Object} { colors: [] | Object }
+     */
+    async get({ latest }) {
+        if (!latest) {
             try {
-                const colorsRef = await readColors(this.src);
-                return colorsRef;
+                const [alt, res] = await readThemeFile(this.src);
+                return alt;
             } catch (err) {
                 if (err.code !== 'ENOENT') {
                     console.log(err);
@@ -99,12 +129,15 @@ class ColorThemeReference {
         }
 
         try {
-            const data = await getReferenceSchema({
-                url,
-                encodeType,
+            const data = await this.getThemeColorReference({
+                url: this.url,
+                encodeType: this.encodeType,
             });
-            const writeData = await fsPromises.writeFile(this.src, data);
-            return Promise.all([data, writeData]);
+            // Create cache of theme-color reference
+            fsPromises.writeFile(this.src, data);
+            // Create JSON object
+            const colorRef = await JSON.parse(data);
+            return colorRef;
         } catch (err) {
             if (err && err.message) {
                 console.log(err.message);
@@ -115,22 +148,85 @@ class ColorThemeReference {
 }
 
 class TestColorThemes {
-    constructor() {
-        this.src = [];
+    constructor({ colors, count }) {
+        this.reference = colors;
+        this.count = count;
     }
     async initialize() {
-        try {
-            this.src = await getThemesDir();
-            if (!this.src.length) {
-                throw "Couldn't find any valid color theme. Please, make sure that your theme file name ends with '-color-theme.json'.";
-            }
-            console.log('Found themes:', this.src);
-        } catch (err) {
-            if (err && err.message) {
-                console.log(err.message);
-            }
-            return Promise.reject(err);
+        const themesAvailiable = await getThemesDir();
+
+        // Not themes found? return error.
+        if (themesAvailiable.length === 0) {
+            console.log(
+                'No valid color theme file found. Make sure the file name ends with *-color-theme.json then test again.',
+            );
+            process.exit(1);
         }
+
+        console.log('Initialize themes colors tokens test...');
+
+        /**
+         *  Loop through each theme inside 'theme/' folder
+         *  @returns List with all themes that passed the tests
+         */
+        const themes = themesAvailiable.filter((dir) => {
+            console.log(`\n @ ${dir.slice(dir.lastIndexOf('themes'))}\n`);
+
+            // Read data from theme
+            const fileData = readFileSync(dir, 'utf-8');
+
+            // Create JSON Object
+            const alt = JSON.parse(fileData);
+
+            // Get all key names inside { colors }
+            const theme = Object.keys(alt.colors);
+
+            const result = {
+                pass: 0,
+                invalid: 0,
+                rate: 0,
+            };
+
+            theme.forEach((key) => {
+                if (this.reference.includes(key)) {
+                    result.pass = result.pass + 1;
+                } else {
+                    result.invalid = result.invalid + 1;
+                }
+            });
+
+            result.rate = (result.pass / this.count) * 100;
+
+            console.log(`  - Total: ${theme.length} tokens.`);
+            console.log(`  - Invalid: ${result.invalid} tokens.`);
+            console.log(
+                `  - Utilized: ${result.rate.toFixed(2)}% of ${
+                    this.count
+                } tokens available in the color-theme references.`,
+            );
+
+            return result.invalid > 0 ? false : true;
+        });
+
+        console.log('\nFinished.\n');
+
+        // If not all available themes passed the test return error
+        if (themes.length !== themesAvailiable.length) {
+            console.log(
+                `Only ${(themes.length / themesAvailiable.length).toFixed(
+                    2,
+                )}% of ${
+                    themesAvailiable.length
+                } themes passed the test. Test failed.\n`,
+            );
+            process.exit(1);
+        }
+
+        // Else if all available themes passed the test:
+        console.log(
+            `No unknown or invalid tokens found. Test finished successfully.\n`,
+        );
+        process.exit(0);
     }
 }
 
