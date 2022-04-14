3d858868c35415f7acfa90d35b2cf7382bd81806
Eduardo M. Santos <99996904+santosned@users.noreply.github.com>
2022-04-14 15:28:35 -0300

    build: update args and line comments.

--- scripts/utils/ThemeDevUtils.js
+++ scripts/utils/ThemeDevUtils.js
@@ -3,8 +3,18 @@
 const fsPromises = require('fs/promises');
 const { join } = require('path');
 
-const getThemesUrl = (url) => join(__dirname, '../../themes/', url);
+/**
+ * 
+ * @param {string} filename Theme file name with file extension
+ * @returns Full path to the theme file
+ */
+const getThemesUrl = (filename) => join(__dirname, '../../themes/', filename);
 
+/**
+ * 
+ * @param {string} url Path to theme file
+ * @returns {object} { label: <string>, uiTheme: <string>, dist: <string>, src: <string> }
+ */
 async function getThemeInfo(url) {
     try {
         const packageInfo = await fsPromises.readFile(url, 'utf-8');
@@ -27,6 +37,11 @@ async function getThemeInfo(url) {
     }
 }
 
+/**
+ * 
+ * @param {string} url Path to theme schema file
+ * @returns {string} Theme schema data
+ */
 async function readThemeSchema(url) {
     try {
         const getData = await fsPromises.readFile(url, 'utf-8');
@@ -36,6 +51,12 @@ async function readThemeSchema(url) {
     }
 }
 
+/**
+ * 
+ * @param {string} url Path to theme file
+ * @param {string} data Theme schema data which will be write
+ * @returns {undefined}
+ */
 async function writeThemeSchema(url, data) {
     try {
         const writeData = await fsPromises.writeFile(url, data);
