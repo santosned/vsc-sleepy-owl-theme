cda96513006d6eb89326278d410a87eee2706eb1
Eduardo M. Santos <99996904+santosned@users.noreply.github.com>
2022-04-15 05:35:13 -0300

    build: add '!cast' custom jsYaml type
    Now it's possible to work with RGB or HEX colors in the yml file. Cast
    allows customization of color lightness or alpha channel easily.

--- scripts/generate.js
+++ scripts/generate.js
@@ -6,23 +6,28 @@
 
 const jsYaml = require('js-yaml');
 
-const { readThemeSchema, writeThemeSchema } = require('./utils/ThemeDevUtils');
+const {
+    readThemeSchema,
+    writeThemeSchema,
+    ColorsTool,
+} = require('./utils/ThemeDevUtils');
 
 /**
- * 
+ *
  * @param {string} src Path to theme schema file
- * @returns 
+ * @returns
  */
-async function getThemeSchema(src) {
+async function getThemeSchema(src, customType) {
     try {
         const yamlData = await readThemeSchema(src);
-
+        const CUSTOM_SCHEMA = jsYaml.DEFAULT_SCHEMA.extend(customType);
         /**
          * Parses yaml file into either a plain object, a string, a number, null or
          * undefined, or throws YAMLException on error.
          */
+
         let jsonData = await jsYaml.load(yamlData, {
-            schema: jsYaml.DEFAULT_SCHEMA,
+            schema: CUSTOM_SCHEMA,
             json: true,
         });
 
@@ -40,19 +45,43 @@ class Transpiler {
         let { tabWidth } = { ...args };
         this.tabWidth = typeof tabWidth === 'number' ? tabWidth : 2;
         this.replacer = (key, value) => {
-            // Exclude json entry if key is either name, author, or version.
-            if (key === 'name' || key === 'author' || key === 'version') {
+            // Key names that should be exclude from the theme file *.json
+            const blackList = ['name', 'author', 'version', 'base'];
+
+            // Exclude json entry if key is in the blacklist
+            if (blackList.includes(key)) {
                 return undefined;
             }
+
             // Exclude json entry if value is null or empty string
             if (value === null || !value) {
                 return undefined;
             }
+
             return value;
         };
+
+        this.jsYaml_ct_cast = new jsYaml.Type('!cast', {
+            kind: 'mapping',
+            resolve: (args) => {
+                return args || {};
+            },
+            construct: (args) => {
+                if (!args.color) return null;
+                const colorsTool = new ColorsTool(
+                    args.color || false,
+                    args.alpha || false,
+                    args.light || false,
+                );
+                return colorsTool.cast();
+            },
+            represent: (args) => {
+                return args;
+            },
+        });
     }
     /**
-     * 
+     *
      * @param {object} options {dist: <string>, src: <string>}
      * @param {function} callback
      */
@@ -70,7 +99,7 @@ class Transpiler {
             throw `generate(): Invalid options 'dist: <string>'. Received '${dist}'.`;
         }
 
-        getThemeSchema(src)
+        getThemeSchema(src, this.jsYaml_ct_cast)
             .then(async ([jsonData, yamlData]) => {
                 try {
                     jsonData = await JSON.stringify(
