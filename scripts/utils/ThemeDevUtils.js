b392102898b817609ece389674ccd40db03a7d6e
Eduardo M. Santos <99996904+santosned@users.noreply.github.com>
2022-04-15 05:28:58 -0300

    build: add ColorsTool feature
    Improve the customization and development of themes. Compatibility with
    rgb and rgba, plus customization of color lightness and alpha channel.

--- scripts/utils/ThemeDevUtils.js
+++ scripts/utils/ThemeDevUtils.js
@@ -8,14 +8,14 @@ const fsPromises = require('fs/promises');
 const { join } = require('path');
 
 /**
- * 
+ *
  * @param {string} filename Theme file name with file extension
  * @returns Full path to the theme file
  */
 const getThemesUrl = (filename) => join(__dirname, '../../themes/', filename);
 
 /**
- * 
+ *
  * @param {string} url Path to theme file
  * @returns {object} { label: <string>, uiTheme: <string>, dist: <string>, src: <string> }
  */
@@ -42,7 +42,7 @@ async function getThemeInfo(url) {
 }
 
 /**
- * 
+ *
  * @param {string} url Path to theme schema file
  * @returns {string} Theme schema data
  */
@@ -56,7 +56,7 @@ async function readThemeSchema(url) {
 }
 
 /**
- * 
+ *
  * @param {string} url Path to theme file
  * @param {string} data Theme schema data which will be write
  * @returns {undefined}
@@ -70,4 +70,153 @@ async function writeThemeSchema(url, data) {
     }
 }
 
-module.exports = { getThemeInfo, readThemeSchema, writeThemeSchema };
+class ColorsTool {
+    constructor(color, alpha, light) {
+        const valueIsIntenger = (value) => {
+            if (typeof value === 'number') return value;
+            return false;
+        };
+
+        this.alpha = valueIsIntenger(alpha); // Check if alpha channel should be adjusted
+        this.light = valueIsIntenger(light); // Check if light adjust should be used
+
+        // Remove any empty space from color value
+        color = color.replace(/\s/g, '');
+
+        this.color = color; // Store color value
+
+        // Check if color follows the HEX format
+        this.matchHex = color.match(/^#((\d|\w){6}|(\d|\w){8})$/);
+
+        // Check if color follows the RGB format
+        this.machRgb = color.match(/^rgb\((\d{1,}\,){2}(\d{1,}){1}\)$/);
+
+        // Check if color follows the RGBA format
+        this.matchRgba = color.match(
+            /^rgba\((\d{1,}\,){3}((\d\.\d{1,})|(\d{1}))\)$/,
+        );
+    }
+
+    cast() {
+        let colorValue = null;
+
+        if (this.matchHex) {
+            colorValue = this.isHex(this.color, this.alpha);
+        } else if (this.machRgb) {
+            colorValue = this.isRgb(this.color, this.alpha);
+        } else if (this.matchRgba) {
+            colorValue = this.isRgba(this.color, this.alpha);
+        }
+
+        // Handle extra options after checking color value
+        if (colorValue !== null) {
+            if (this.light !== false) {
+                colorValue = lightAdjust(colorValue, this.light);
+            }
+        }
+
+        return colorValue;
+    }
+
+    // Handle if color value follows the HEX format
+    isHex(hex, percentage) {
+        // Return color if no alpha channel was passed.
+        if (percentage === false) return hex;
+
+        // Check if HEX color has alpha channel already.
+        if (hex.length === 9) {
+            // If so return it replaced with alpha channel percentage
+            hex = hex.replace(/[aA0-zZ9]{2}$/g, '');
+            return hex + this.percentToHex(percentage);
+        }
+
+        // Check if HEX does not has alpha channel.
+        if (hex.length === 7) {
+            // If so return it with alpha channel percentage
+            return hex + this.percentToHex(percentage);
+        }
+    }
+
+    // Handle if color value follows the RGB format
+    isRgb(rgb, percentage) {
+        // Get RGB values into a list
+        rgb = rgb.replace(/rgb\(|\)/g, '').split(',');
+
+        if (percentage === false) {
+            return this.rgbToHex(rgb); // Return hex color without alpha channel.
+        }
+        // Return HEX color with alpha channel
+        return this.rgbToHex(rgb) + this.percentToHex(percentage);
+    }
+
+    // Handle if color value follows the RGBA format
+    isRgba(rgba, percentage) {
+        if (percentage === false) {
+            // Get RGBA values into a list
+            rgba = rgba.replace(/rgba\(|\)/g, '').split(',');
+            return this.rgbaToHex(rgba); // Return HEX color without alpha channel.
+        }
+        // Remove RGB alpha channel and get RGB values into a list
+        const rgb = rgba.replace(/^rgba\(|\,(.){1,3}\)$/g, '').split(',');
+        // Return HEX color with alpha channel
+        return this.rgbToHex(rgb) + this.percentToHex(percentage);
+    }
+
+    // Adjust hex color lightness
+    lightAdjust(hex, amount) {
+        let alpha = hex.length === 9 ? hex.slice(7, 9) : '';
+
+        let r = hex.slice(1, 3);
+        let g = hex.slice(3, 5);
+        let b = hex.slice(5, 7);
+
+        r = parseInt(r, 16) + amount;
+        g = parseInt(g, 16) + amount;
+        b = parseInt(b, 16) + amount;
+
+        r = Math.min(255, Math.max(0, r)).toString(16).padStart(2, '0');
+        g = Math.min(255, Math.max(0, g)).toString(16).padStart(2, '0');
+        b = Math.min(255, Math.max(0, b)).toString(16).padStart(2, '0');
+
+        return '#' + r + g + b + alpha;
+    }
+
+    // Convert alpha channel percentage into HEX
+    percentToHex(alpha) {
+        alpha = parseInt((alpha * 255) / 100).toString(16);
+        alpha = alpha.padStart(2, '0');
+        return alpha;
+    }
+
+    // Convert RGB color into HEX color
+    rgbToHex(rgb) {
+        // Get rgb(a) values into one array of strings
+        const hexColor = rgb.map((color, index) => {
+            return parseInt(color).toString(16).padStart(2, '0');
+        });
+
+        return '#' + hexColor.join('');
+    }
+
+    // Convert RGBA color into HEX color
+    rgbaToHex(rgba) {
+        // Get rgb(a) values into one array of strings
+        const hexColor = rgba.map((color, index) => {
+            if (index === 3) {
+                // rgba(0, 1, 2, [3])
+                const alpha = parseFloat(color);
+                return percentToHex(alpha > 1.0 ? 100 : alpha * 100);
+            }
+            return parseInt(color).toString(16).padStart(2, '0');
+        });
+
+        return '#' + hexColor.join('');
+    }
+}
+
+module.exports = {
+    getThemeInfo,
+    readThemeSchema,
+    writeThemeSchema,
+    ColorsTool,
+};
