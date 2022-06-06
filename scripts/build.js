a78699279e9ce71c80e35e8fda8c40cd6cafd27a
Eduardo M. Santos <99996904+santosned@users.noreply.github.com>
2022-06-06 16:17:53 -0300

    build: support building multiple themes

--- scripts/build.js
+++ scripts/build.js
@@ -8,24 +8,24 @@ const transpiler = require('./generate');
 const { log } = require('./lib/console');
 const { metadata } = require('./lib/snippet');
 
+async function build(themeInfo) {
+    try {
+        log.build.init(themeInfo.name);
+        await transpiler.generate(themeInfo);
+        log.build.ready(themeInfo);
+        return;
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+}
+
 metadata
     .getThemeInfo(process.env.npm_package_json)
     .then((themeInfo) => {
-        log.build.init(themeInfo);
-
-        transpiler.generate(themeInfo, (err) => {
-            // This will be called every time a error occurs
-            if (err) {
-                // Log the error message if any
-                if (err.message) console.log(err.message);
-                process.exit(1);
-            }
-
-            if (err === undefined) {
-                log.build.ready();
-                process.exit(0);
-            }
-        });
+        for (const theme of Object.values(themeInfo)) {
+            build(theme);
+        }
     })
     .catch((err) => {
         if (err && err.message) {
